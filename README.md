fddeba4a2ad90b9297b758b807eac0cd496fb4b5
John Alison <johnda102@gmail.com>
2023-03-31 10:26:38 -0400

    Update

--- README.md
+++ README.md
@@ -26,4 +26,43 @@ https://docs.google.com/forms/d/e/1FAIpQLSc6fYKz6bnDXkWObLDc6bIjRYMrxTV5CZGc1lBl
 
 # To do
    * Alow for a list of vetos
+   * allow for and list
    * allow option to turn off hints
+   * which /
+   * If you are giving a number as a percentage with an uncertainty in the text, use parentheses around the number and its uncertainty. Example: "The muon efficiency is $(94.3 \pm 1.3)\%$."
+   * If you are giving a range of percentages, use "2--3%" not "2%--3%". Notice the use of the double hyphen to indicate a range, as discussed in the section on hyphens.
+
+
+
+Not added:
+If there is no number to the left of the symbol, use curly braces around the symbol to obtain the correct spacing, e.g. "${>} 60\GeV$", ${\pm}3$", "${\approx}4$". The curly braces let LaTeX know that the normally binary operator is spaced as a unary operator. This will leave the correct spacing between the operator and the number.
+
+Only use the equal symbol "=" with integer numbers, not with real numbers. It is correct to write $10 < \pt < 20\GeV$, $\abs{y} < 1.2$. It is incorrect to write "in two bins, $\abs{y} < 0.6$ and $0.6 \leq \abs{y} < 1.2$". It is mathematically impossible that an event has a particle with \abs{y} = 0.6; the rapidity being a real number, it will always be slightly larger or slightly smaller than 0.6 or any other specific number.
+
+In ranges, do not apply the same number of digits unless they have a real meaning. Write, for instance, $0.9 < \abs{y} < 1.25$ and not $0.90 < \abs{y} < 1.25$. It does not make sense to write, for instance, $\abs{y} < 1.50$, because the edges of the bins are decided by the analysts and, hence, have "infinite precision"; they are not the result of a measurement and there is no information content in the number of "zeros". One could just as well write "1.25000000000". It is correct (and important) to write 80.0\invfb, for instance. It is useless and misleading to write "\pt bins of edges $10.0, 12.5, 15.0, 17.5 and 20.0\GeV$.
+
+# Significant Digits:
+In CMS papers, the standard is to use at most two significant digits for all quoted uncertainties, unless only one significant digit is appropriate.
+The precision quoted for an uncertainty should match the precision quoted for the central value associated with it and the precision of any other associated quoted uncertainties.
+Examples:
+
+27.4 ± 0.1 (stat) ± 2.1 (syst): the measurement and the statistical uncertainty are given to only one digit after the decimal point to match the precision of the systematic uncertainty;
+27.40 ± 0.14 (stat) ± 0.85 (syst): now the measurement and the statistical uncertainty are given to two digits after the decimal point to match the precision of the systematic uncertainty;
+27.4 ± 1.3 (stat) ± 0.2 (syst): the systematic uncertainty is given with only 1 significant digit, to match the precision of the statistical uncertainty.
+
+If an uncertainty is so small compared to the other uncertainties so that this guideline cannot be followed, do not quote the value of that uncertainty. Just say it is negligible compared to the other uncertainties. For example, if the statistical uncertainty in a measurement is ±0.001 and the systematic uncertainty is 2.2, we would quote the result as
+
+Notice our convention for labeling the statistical and systematic uncertainties: "(stat)" and "(syst)", without a period at the end of each abbreviation and with a space before and after the parentheses.
+
+
+
+# Acronyms
+
+An acronym (excluding MC names) should not be defined unless the associated phrase is used at least three times in the text. An acronym should be separately defined in the abstract and in the main body of the text, and should be defined in the abstract only if it will be used at least two more times in the abstract. Same thing for the summary: all used acronyms should be redefined, and introduced only if they will be used at least twice more. The acronym should always directly follow the definition, be placed inside parentheses, and may have an "s" added (for example "PDFs") to match the defining phrase. If the "s" is added in the definition, then the acronym without an "s" does not have to be defined also. Similarly, if the acronym without an "s" is defined, then the acronym with an "s" can be used without also defining it.
+
+
+Do not start a sentence with a symbol.
+
+
+Refer to your result as a concrete observable
+Do not use expressions like "Measurements of \ttbar production are obtained..." or "Limits on the pair production of squarks and gluinos are derived..." because "production" is not an observable. Instead, refer to a concrete observable such as a cross section, a polarization, etc. If it is appropriate to be more generic, you could refer to "properties." Thus, it would be correct to state "Measurements of \ttbar production properties are obtained ..." or "Limits on the cross sections for the pair production of gluinos and squarks are derived..."
--- proofRead.py
+++ proofRead.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import hashlib
+import re
 
 #filename = "mixedData.tex"  # change to the name of your text file
 
@@ -16,13 +17,20 @@ with open(".proofReadingIgnore","r") as file:
 
 class check:
 
-    def __init__(self, match, veto=None, hint=None):
+    def __init__(self, match, veto=None, hint=None, ignoreCase=True):
         self.match = match
         self.veto = veto
         self.hint = hint
+        if ignoreCase:
+            self.regex = re.compile(self.match, flags=re.IGNORECASE)
+        else:
+            self.regex = re.compile(self.match)
+
         
     def __call__(self, line, lineNumber, filename):
-        if (self.match in line):
+
+        if self.regex.findall(line):         
+        #if (self.match in line):
 
             if self.veto:
                 if (self.veto in line): return
@@ -36,56 +44,66 @@ class check:
             print(f"\t > {line}")
             if self.hint: print(f"\t\t {self.hint}\n")
 
+
+            
     
 checkList = [
     check("systematics", veto=":systematics"),
     check("kinematics"),
     check("dataset"),
+    check("uncertainty on"),
     check("cut"),
     check("Monte Carlo"),
     check("statistics", hint='Do not use the term "statistics" as a substitute for "amount of data". Write "the larger sample of events" rather than "the higher statistics", and "improve the statistical precision" rather than "improve the statistics".'),
     check("due to", hint='try replacing "due to" by "caused by". If the sentence still works, then your use of "due to" is correct. However, if replacing "due to" by "because of" works even better, \n\t\t\tthen "due to" is not correct. Replace it with "because of.'),
-    check("i.e."),
-    check("e.g."),
-    check("allows"),
+    check(r"i\.e\."),
+    check(r"e\.g\."),
+    check("allows", hint='The phrases "allows to do something" and "allows to use" are incorrect. Alternative phrasings with the intended meaning are "allows something to be accomplished" or "allows the use of".'),
     check("facilitates"),
+    check("charged tracks"),
     check("permits"),
+    check("fake"),
+    check("error"),
+    check("cross section times branching fraction"),
+    #check("higgs"),
     check("data is"),
-    check("discriminator"),
+    check(r"(?<!^)Figure",ignoreCase=False),
+    check(r"(?<!^)Equation",ignoreCase=False),
+    #check("Figure"),
+    check("discriminator", hint='While both words mean "a distinguishing characteristic", “discriminant” is more commonly used in a scientific or mathematical context, and “discriminator” is used in more general parlance. Thus, we should definitely use the former in CMS publications.'),
+    check(r" [ZWHb] "),
+    check(r" \\GeV"),
+    check(r" \\MeV"),
+    check(r" \\%"),
     #check("which",veto="in which",hint='"Which" should be used for a nonrestrictive clause, i.e., a parenthetical comment that could be removed from the sentence without changing its meaning. A nonrestrictive clause is always offset in commas. In contrast, "that" should be used to introduce a restrictive clause, and there should be no comma(s). A restrictive clause qualifies the item discussed before it and thus changes the meaning of the sentence.')
 ]
 
 
-noNos = ["evidences","informations","importances", "performances", "significances"]
+noNos = ["evidences","informations","importances", "performances", "significances", "It's", "it's","associated to", "stat\.", "sys\.","syst\.","C\.L\."
+         "let's", "don't", "can't", "won't", "it's", "shouldn't",
+         ]
 for w in noNos:
     checkList.append(check(w))
-
-#checks["dataset"] = warn
-#checks["kinematics"] = warn
-#checks["cut"] = warn
-#checks["Monte Carlo"] = warn
-#checks["statistics"] = warn
-#checks["systematics"] = warn
-#checks["due to"] = warn
-#checks["i.e."] = warn
-#checks["e.g."] = warn
-#checks["allows"]= warn
-#checks["facilitates"]= warn
-#checks["permits"]= warn
-#checks["data is"]= warn
-#checks["discriminator"] = warn
 
 
 noHyphens = []
 with open("noHyphens.txt", "r") as file:
     for line in file:
         noHyphens.append(line.strip())
-        #print(f"{line.strip()!r}")
+
 
 with open("noHyphensTwoWords.txt", "r") as file:
     for line in file:
         noHyphens.append(line.strip())
 
+hyphens = []
+hyphensTest = []
+with open("hyphens.txt", "r") as file:
+    for line in file:
+        hyphens    .append(line.strip())
+        hyphensTest.append(line.strip().replace("-"," "))
+
+        
         
 def processFile(filename):
     print(f"{filename}\n")
@@ -96,20 +114,38 @@ def processFile(filename):
             lineNumber += 1 
             if line[0] == "%": continue
 
-            
+            for hyphenWord, hyphenWordTest in zip(hyphens, hyphensTest):                
+                if hyphenWordTest in line:
+                    hashID = hashlib.sha1((hyphenWord+":"+line).encode("UTF-8")).hexdigest()[:10]
+                    if hashID not in ignoreHashes: 
+                        print(f"\tWarning {hyphenWord}:{lineNumber}\t{hashID}")
+                        print(f"\t\tShould be a hyphen(s) in line \n\t\t {line} ")
+
+
+                    #print(hyphenWord)
+                    #warn("Should be a hyphen in "+
+
+
             for w in line.split():
 
+
+                #
+                #  IF word has hyphen check if OK
+                #
                 if "-" in w:
                     wNoHyphen = w.replace("-","") 
                     if wNoHyphen in noHyphens:
-                        print(wNoHyphen)
-                        warn("Should be no hyphen in "+w,line,lineNumber, filename)
+                        hashID = hashlib.sha1((wNoHyphen+":"+line).encode("UTF-8")).hexdigest()[:10]
+                        if hashID not in ignoreHashes: 
+                            print(f"\tWarning {w}:{lineNumber}\t{hashID}")
+                            print(f"\t\tShould be no hyphen in {w} ")
 
+                    
             for c in checkList:
                 c(line,lineNumber, filename)
                 #if c in line:
                 #    checks[c](c,line,lineNumber, filename)
-                        
+
                 
     
 
